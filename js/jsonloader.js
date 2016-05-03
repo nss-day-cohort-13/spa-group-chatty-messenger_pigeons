acf07ddc2c37d13b278d45415bbad6f4e0cc8852
jbrooks036 <joannbrooks036@gmail.com>
2016-05-03 16:16:10 -0500

    general clean up

--- js/jsonloader.js
+++ js/jsonloader.js
@@ -2,20 +2,12 @@ var chatty = (function () {
   console.log("now in jsonloader.js");
 
   var messagesArray = [];
-  console.log("5: messages = ", messagesArray);
 
   var loader = new XMLHttpRequest();
 
   loader.addEventListener("load", function () {
     // Set the value of the private array
     messagesArray = JSON.parse(this.responseText).jsonMessages;
-    console.log("12: messagesArray = ", messagesArray);
-
-    // Invoke the callback function so that the caller knows
-    // that the process is complete. Make sure to pass the 
-    // carnivore array as an argument.
-    
-    //    callbackToInvoke(messagesArray);
   });
 
   loader.open("GET", "../messages.json");
