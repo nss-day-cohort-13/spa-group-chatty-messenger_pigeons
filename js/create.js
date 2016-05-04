43f4d099669e5012c73af0794c7641d975ed3931
Megan Ford <meg.c.ford@gmail.com>
2016-05-04 09:28:19 -0500

    enter key works to add to array vi
    also connected enterkey js to index.html.

--- js/create.js
+++ js/create.js
@@ -1,30 +1,40 @@
 var chatty = (function(chatty) {
+  
+
   //this is where the messages will come in and out. 
   var messageArray = [];
 
-  //this function runs when the enter key is pressed in the DOM input element.
-  chatty.enterKey = function(keyup) {
-    if (keyup.keycode === 13) {
-      injectMessage();
-    } else {
-      mirrorMessage();
-    }
-  },
 
-  chatty.addMessageToArray = function(message) {
-    messageArray.push(message);
-  }
 
   chatty.injectMessage =function() {
     console.log("enter key was pressed");
     //here is where I'll run the function to push the value of the input element into the array. as a string.
-    this.addMessageToArray(blerblerbler.value);
+    this.addMessageToArray(messageInput.value);
   },
 
   //probably won't need this function but just in case. 
   chatty.mirrorMessage = function() {
     console.log("another key was pressed"); 
+  },
+  
+  //this function runs when the enter key is pressed in the DOM input element.
+  chatty.enterKey = function(keyup) {
+    if (keyup.keyCode === 13) {
+      chatty.injectMessage();
+    } else {
+      chatty.mirrorMessage();
+    }
+  },
+
+  chatty.addMessageToArray = function(message) {
+    messageArray.push(message);
+    console.log("what the array looks like", messageArray);
+    messageInput.value = "";
   }
+
+ 
+
+  
 
   return chatty;
 
@@ -32,3 +42,5 @@ var chatty = (function(chatty) {
 
 
 
+var messageInput = document.getElementById("message-input");
+messageInput.addEventListener("keyup", chatty.enterKey);
