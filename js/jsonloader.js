4aa9a4cff77a6ae06649455381f0ca5f90fbbc66
Megan Ford <meg.c.ford@gmail.com>
2016-05-11 16:00:48 -0500

    json loader jquerified and works

--- js/jsonloader.js
+++ js/jsonloader.js
@@ -1,46 +1,40 @@
 // this module loads the messages 
 //from the JSON file into messageArray
 
+"use strict"; 
+
+
 var chatty = (function (chatty) {
 
-  // an array to hold messages wrapped as POJOs, first step from JSON
-  var messageObjectsArray = [];
+  chatty.loadStockMessages = function() {
+    return new Promise((resolve, reject) =>{
 
-  // array that will contain messages as strings, no more objects
-  var messagesArray = [];
+      $.ajax({
+        url: "./../data/messages.json"
+      }).done(function(stockMessages) {
+        resolve(stockMessages);
+      }).fail(function(xhr, status, error){
+        reject(error);
+      }); //end of AJAX
 
-  // create a request to retrieve JSON 
-  var loader = new XMLHttpRequest();
+    });//end of promise
+  };//end of loadStockMessages
 
-  // set up loader with callback function
-  loader.addEventListener("load", function () {
+  chatty.loadStockMessages()
+  .then
+    (function(stockMessages){
+      chatty.injectStockMessages(stockMessages);
+    }),
+    (function(error){
+      //noted that gulp here would prefer a function call. 
+      console.log("error", error);
+    });
 
-    chatty.disableButton();
-    
-    // get the json and put it into a local (private) array 
-    messageObjectsArray = JSON.parse(this.responseText).jsonMessages;
+chatty.injectStockMessages = function(stockMessages){
+  //put the messages through a loop- the same as the 'enter press' loop?
+  console.log("stock messages object",stockMessages );
+};
 
-    // strip off the object wrappers from the messages
-    messagesArray = messageObjectsArray.map(extractMessageFromObject);
-
-    // transfer the messages into the private array that create iife can access
-    chatty.setMessageArray(messagesArray);
-
-    // post all the messages onto the webpage
-    chatty.loopThroughArray();
-
-  });
-
-  // now do the actual load from JSON file
-  loader.open("GET", "../messages.json");
-  loader.send();
-
-  // helper function to put clean messages (no object wrappers) into array
-  function extractMessageFromObject (obj) {
-    return obj.message;
-  }
-
-  // all done
   return chatty;
 
 }(chatty || {}));
